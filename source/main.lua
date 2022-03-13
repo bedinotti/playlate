9572ec739464ee72257e856bfe683a8e1bed3b3d
Chris Downie <cdownie@gmail.com>
2022-03-13 13:22:31 -0700

    Add comment for default code in playdate.update

--- source/main.lua
+++ source/main.lua
@@ -8,6 +8,7 @@ local gfx = playdate.graphics
 gfx.setColor(gfx.kColorBlack)
 
 function playdate.update()
+    -- Example code. Draw a full-screen rectangle and the frames per second
     gfx.fillRect(0, 0, 400, 240)
     playdate.drawFPS(0,0)
 end
