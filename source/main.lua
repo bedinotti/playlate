68683ad5a400b30a6594eef0c365403b2d39d941
Chris Downie <cdownie@gmail.com>
2022-03-13 13:26:02 -0700

    Add sprite.update call in playdate.update

--- source/main.lua
+++ source/main.lua
@@ -11,4 +11,9 @@ function playdate.update()
     -- Example code. Draw a full-screen rectangle and the frames per second
     gfx.fillRect(0, 0, 400, 240)
     playdate.drawFPS(0,0)
+
+    -- Update and draw all sprites. Calling this method in playdate.update
+    -- is generally what you want, if you're using sprites.
+    -- See https://sdk.play.date/1.9.1/#f-graphics.sprite.update for more info
+    gfx.sprite.update()
 end
