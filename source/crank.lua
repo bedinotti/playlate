804465540542b7698db0a89fd11923960d3ac12f
Chris Downie <cdownie@gmail.com>
2022-03-14 16:07:42 -0700

    Add doccomments for each method in crank.lua

--- source/crank.lua
+++ source/crank.lua
@@ -1,16 +1,21 @@
 -- All the callbacks for the crank.
--- See https://sdk.play.date/1.9.0/Inside%20Playdate.html#_crank_callbacks for more information
+-- See https://sdk.play.date/1.9.1/Inside%20Playdate.html#_crank_callbacks for more information
 
 local prefix = "[Crank] "
 
+--- Called when the crank moves.
+--- @param change number How much the crank moved, in degrees. Negative values are anti-clockwise.
+--- @param acceleratedChange number How much the crank moved, scaled by how fast it was moved.
 function playdate.cranked(change, acceleratedChange)
 	print(prefix .. string.format("Crank rotated %.4f degrees clockwise (%.4f degrees accelerated).", change, acceleratedChange))
 end
 
+--- Called when the crank is docked.
 function playdate.crankDocked()
 	print(prefix .. "Crank docked.")
 end
 
+--- Called when the crank is undocked.
 function playdate.crankUndocked()
 	print(prefix .. "Crank undocked.")
 end
